f3037997b79e8fe8248a24a62e3f47e495346b90
somethinrother <jamesneilson4@gmail.com>
2019-11-20 16:42:41 -0500

    Method can find the first letter

--- secret_string.rb
+++ secret_string.rb
@@ -9,28 +9,43 @@ triplets_1 = [
  ['w','h','s']
 ]
 
-def find_pairs_from_triplets(triplets)
-  pairs = []
-  triplets.each do |triplet|
-    pairs << [triplet[0], triplet[1]]
-    pairs << [triplet[1], triplet[2]]
+class SecretStringFinder
+  attr_accessor :letters, :pairs
+
+  def initialize(triplets)
+    @pairs = find_pairs_from_triplets(triplets)
+    @letters = @pairs.flatten.uniq
   end
 
-  pairs
+  def find_pairs_from_triplets(triplets)
+    pairs = []
+    triplets.each do |triplet|
+      pairs << [triplet[0], triplet[1]]
+      pairs << [triplet[1], triplet[2]]
+    end
+
+    pairs
+  end
+
+  # First letter would be the only one that is never second in a pair
+  def find_next_letter
+    # loop over letters, and in each loop, run a smaller one
+    next_letter = nil
+
+    @letters.each do |letter|
+      letter_is_valid = true
+
+      @pairs.each do |pair|
+        letter_is_valid = false if letter == pair[1]
+      end
+
+      next_letter = letter if letter_is_valid
+    end
+
+    next_letter
+  end
+  # next letter will also never occur second as soon as the first letter is filtered
+  # Build your string by adding the first letter, then each new one in sequence
 end
 
-# First letter would be the only one that is never second in a pair
-def find_next_letter(pairs)
-  # loop over pairs, and in each loop, run a smaller one
-  # Each smaller loop compares the first letter of the first loop's current iteration
-  # with the second letters of each pair.
-  # There should only be one letter that doesn't match with any second letter
-end
-
-def find_secret(triplets)
-  pairs = find_pairs_from_triplets(triplets)
-end
-# next letter will also never occur second as soon as the first letter is filtered
-# Build your string by adding the first letter, then each new one in sequence
-
-puts find_secret(triplets_1).inspect
+f = SecretStringFinder.new(triplets_1)
