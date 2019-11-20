f08465f6f1d06234608c4709865f351ccd27bbc8
somethinrother <jamesneilson4@gmail.com>
2019-11-19 21:10:56 -0500

    Method to find ordered pairs works

--- secret_string.rb
+++ secret_string.rb
@@ -10,6 +10,21 @@ triplets_1 = [
 ]
 
 # Transform triplets into (unique) pairs to be compared
+def find_pairs_from_triplets(triplets)
+  pairs = []
+  triplets.each do |triplet|
+    pairs << [triplet[0], triplet[1]]
+    pairs << [triplet[1], triplet[2]]
+  end
+
+  pairs
+end
+
+def find_secret(triplets)
+  pairs = find_pairs_from_triplets(triplets)
+end
 # First letter would be the only one that is never second in a pair
 # next letter will also never occur second as soon as the first letter is filtered
 # Build your string by adding the first letter, then each new one in sequence
+
+puts find_secret(triplets_1).inspect
