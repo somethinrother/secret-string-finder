e91fd283a4aa75a60ff53febbc018d022d3809e4
somethinrother <jamesneilson4@gmail.com>
2019-11-19 21:20:04 -0500

    Rough gameplan for next letter method

--- secret_string.rb
+++ secret_string.rb
@@ -9,7 +9,6 @@ triplets_1 = [
  ['w','h','s']
 ]
 
-# Transform triplets into (unique) pairs to be compared
 def find_pairs_from_triplets(triplets)
   pairs = []
   triplets.each do |triplet|
@@ -20,10 +19,17 @@ def find_pairs_from_triplets(triplets)
   pairs
 end
 
+# First letter would be the only one that is never second in a pair
+def find_next_letter(pairs)
+  # loop over pairs, and in each loop, run a smaller one
+  # Each smaller loop compares the first letter of the first loop's current iteration
+  # with the second letters of each pair.
+  # There should only be one letter that doesn't match with any second letter
+end
+
 def find_secret(triplets)
   pairs = find_pairs_from_triplets(triplets)
 end
-# First letter would be the only one that is never second in a pair
 # next letter will also never occur second as soon as the first letter is filtered
 # Build your string by adding the first letter, then each new one in sequence
 
